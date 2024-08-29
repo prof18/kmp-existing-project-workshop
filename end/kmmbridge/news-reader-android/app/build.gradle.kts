85a2f93604986b80845d2d47150c51f0f7d8c95e
Marco Gomiero <marco.gomiero.93@gmail.com>
2024-08-29 21:50:21 +0200

    Update KMMBridge

--- end/kmmbridge/news-reader-android/app/build.gradle.kts
+++ end/kmmbridge/news-reader-android/app/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
 
-    implementation("com.example.newsreaderkmp:shared:0.1.1")
+    implementation("com.example.newsreaderkmp:shared:0.2.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
